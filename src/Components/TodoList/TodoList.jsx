4df9b8e294ece619a44656b65932a9723edb2e5e
armin-bkh <arminbkh0921@yahoo.com>
2022-01-03 01:46:09 +0330

    handle getAsyncTodos action

--- src/Components/TodoList/TodoList.jsx
+++ src/Components/TodoList/TodoList.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   checkTodo,
   deleteTodo,
   editTodo,
+  getAsyncTodos,
 } from "../../features/TodosSlice/TodosSlice";
 import TodoCompleteItems from "../TodoCompleteItems/TodoCompleteItems";
 import TodoForm from "../TodoForm/TodoForm";
@@ -11,8 +12,12 @@ import TodoItem from "../TodoItem/TodoItem";
 
 const TodoList = () => {
   const [edit, setEdit] = useState(null);
-  const { todos } = useSelector((state) => state.todos);
+  const { todos, loading, error } = useSelector((state) => state.todos);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAsyncTodos());
+  }, []);
 
   const removeHandler = (id) => {
     dispatch(deleteTodo({ id }));
@@ -27,25 +32,34 @@ const TodoList = () => {
     setEdit(null);
   };
 
+  if (loading) return <h1>loading...</h1>;
+  if (error) return <h1>{error}</h1>;
+
   return (
     <div>
       <ul>
-        {todos.map((todo) =>
-          todo.id === edit?.id ? (
-            <TodoForm key={todo.id} onSubmit={editHandler} value={todo.value} />
-          ) : (
-            <TodoItem
-              key={todo.id}
-              value={todo.value}
-              checked={todo.checked}
-              onDelete={() => removeHandler(todo.id)}
-              onCheck={() => checkHandler(todo.id)}
-              onEdit={() => setEdit(todo)}
-            />
-          )
-        )}
+        {todos &&
+          todos.length &&
+          todos.map((todo) =>
+            todo.id === edit?.id ? (
+              <TodoForm
+                key={todo.id}
+                onSubmit={editHandler}
+                value={todo.value}
+              />
+            ) : (
+              <TodoItem
+                key={todo.id}
+                value={todo.value}
+                checked={todo.checked}
+                onDelete={() => removeHandler(todo.id)}
+                onCheck={() => checkHandler(todo.id)}
+                onEdit={() => setEdit(todo)}
+              />
+            )
+          )}
       </ul>
-      {todos.length ? (
+      {todos && todos.length ? (
         <TodoCompleteItems />
       ) : (
         <h1 className="title">empty todo</h1>
--- src/features/TodosSlice/TodosSlice.js
+++ src/features/TodosSlice/TodosSlice.js
@@ -1,8 +1,23 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import getTodos from "../../Services/getTodos";
 
 const initialState = {
   todos: [],
+  loading: false,
+  error: null,
 };
+
+export const getAsyncTodos = createAsyncThunk(
+  "todos/getAsyncTodos",
+  async (_, { rejectWithValue }) => {
+    try {
+      const { data } = await getTodos();
+      return data;
+    } catch (error) {
+      return rejectWithValue([], error);
+    }
+  }
+);
 
 const todosSlice = createSlice({
   name: "todos",
@@ -29,6 +44,22 @@ const todosSlice = createSlice({
         (todo) => todo.id === action.payload.id
       );
       selectedTodo.value = action.payload.title;
+    },
+  },
+  extraReducers: {
+    [getAsyncTodos.pending]: (state, action) => {
+      return { ...state, error: null, loading: true, todos: [] };
+    },
+    [getAsyncTodos.rejected]: (state, action) => {
+      return {
+        ...state,
+        error: action.error.message,
+        loading: false,
+        todos: [],
+      };
+    },
+    [getAsyncTodos.fulfilled]: (state, action) => {
+      return { ...state, error: null, loading: false, todos: action.payload };
     },
   },
 });
